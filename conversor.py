0572ebc1f458dc5e97a643196f89245ae9448b25
ale ubeto <alejo.ubeto@gmail.com>
2022-01-26 22:12:29 -0600

    Primeros dos programas: Conversión a dolares

---
+++ conversor.py
@@ -0,0 +1,4 @@
+pesos = float(input('¿Cuántos pesos mexicanos tienes?\n>>>'))
+dolar = 20.80
+conversion = pesos / dolar
+print(f'Tienes ${round(conversion,2)}')
---
+++ conversor2.py
@@ -0,0 +1,4 @@
+dolar = float(input('¿Cuántos dolares tienes?\n>>>'))
+pesos = 0.048
+conversion = dolar / pesos
+print(f'Tienes ${round(conversion,2)}')
