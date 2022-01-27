6171fcf8daa6e573cd310ceb3064bae1bb5e7abe
ale ubeto <alejo.ubeto@gmail.com>
2022-01-27 00:23:22 -0600

    Nuevos pesos para convertir en conversor.py

--- conversor.py
+++ conversor.py
@@ -1,4 +1,18 @@
-pesos = float(input('¿Cuántos pesos mexicanos tienes?\n>>>'))
-dolar = 20.80
-conversion = pesos / dolar
-print(f'Tienes ${round(conversion,2)}')
+menu = '\n💰Bienvenidx al conversor de monedas💰\n1.-Pesos colombianos\n2.-Pesos argentinos\n3.-Pesos mexicanos\n👉 '
+opcion = int(input(menu))
+pesos = float(input('\n¿Cuántos pesos tienes?\n👉 '))
+
+if opcion == 1:
+    dolar = 3930.00
+    print(f'🤑Tienes ${round(pesos/dolar,2)}')
+
+elif opcion == 2:
+    dolar = 104.70
+    print(f'🤑Tienes ${round(pesos/dolar,2)}')
+
+elif opcion == 3:
+    dolar = 20.81
+    print(f'🤑Tienes ${round(pesos/dolar,2)}')
+
+else:
+    print('\n😔Por favor, ingresa una opción válida.')
